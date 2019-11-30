ca921c8e0eac38ec5b50b80cdc387ad3e65042bf
Jon Savas <savasjonathan@gmail.com>
2019-11-30 17:35:27 +0000

    -- added win percentage data for some games

--- nfl_data.py
+++ nfl_data.py
@@ -3,9 +3,16 @@ from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 pandas2ri.activate()
 nfl_scrap_r = importr("nflscrapR")
+
+output_dir = f"{os.getcwd()}/wp_data"
+
+def create_output_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 def get_game_id(home_team, week, year=2019):
     sched_df = nfl_scrap_r.scrape_game_ids(year, weeks=week)
@@ -33,4 +40,13 @@ def plot_wp(df_wp, team):
     plt.show()
 
 
+games = [['LA', 'BAL', 12], ['NE', 'DAL', 12], ['SF', 'GB', 12], ['DAL', 'BUF', 13], ['DET', 'CHI', 13], ['ATL', 'NO', 13]]
 
+create_output_dir(output_dir)
+
+for game in games:
+    week = game[2]
+    home = game[0]
+    away = game[1]
+    wp_df = get_win_percentage_df(get_game_id(home, week))
+    wp_df.to_csv(f"{output_dir}/week-{week}_{home}_{away}_wp.csv")
